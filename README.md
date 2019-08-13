3cb8d8d30b4a8a17021d79917e221b51382edd2d
ohsyln <sylnnnn@gmail.com>
2019-08-13 22:24:44 +0800

    sic

---
+++ README.md
@@ -0,0 +1,54 @@
+ # topgainers-premarket-scanner
+
+Got sick of manually refreshing my free premarket scanners. This non-API web scraper pulls data from free (delayed) premarket scanner(s) for top gainers, further filtering them with custom parameters such as `Change %`, `Last`, `Volume`, `Float`.
+
+Uses urllib (web request) and BeautifulSoup (HTML parsing).
+
+Uses [thestockmarketwatch.com](http://thestockmarketwatch.com/markets/pre-market/today.aspx) (premarket scanner) and [finance.yahoo.com](https://finance.yahoo.com) (float info).
+
+### Prerequisites (MacOS/Linux)
+
+Python3
+
+### Installing
+
+Clone to your local directory:
+
+```
+git clone https://github.com/ohsyln/topgainers-premarket-scanner
+cd topgainers-premarket-scanner
+```
+
+Activate py virtual environment:
+
+```
+source pyvenv/bin/activate
+```
+
+Install BeautifulSoup and Texttable:
+
+```
+pip3 install beautifulsoup4 texttable
+```
+
+## Run script
+
+Edit parameters in `scanner.py` with a text editor:
+
+```
+PERIOD = 30 # seconds per refresh
+GAINS_PERCENT = 10 # minimum change %
+MAX_LAST = 50 # maximum price in dollars
+FLOAT_BTW = [0,150000000] # size of public float
+MIN_VOLUME = 100000 # minimum volume
+```
+
+Save changes and run script
+
+```
+python3 scanner.py
+```
+
+## License
+
+MIT
---
+++ scanner.py
@@ -0,0 +1,98 @@
+from urllib.request import urlopen, Request
+from bs4 import BeautifulSoup
+import texttable as tt
+import datetime, time
+
+PERIOD = 15 # seconds per refresh
+GAINS_PERCENT = 10 # at least how many percent gain
+MAX_LAST = 100 # maximum price
+FLOAT_BTW = [2000000,1500000000000] # size of float
+MIN_VOLUME = 10000 # minimum volume
+
+YAHOO_FIN = 'https://finance.yahoo.com/quote/{}/key-statistics?p={}'
+QUOTE_PAGE = 'http://thestockmarketwatch.com/markets/pre-market/today.aspx'
+#QUOTE_PAGE2= 'https://marketchameleon.com/Reports/PremarketTrading'
+GRAY = '\033[1;30;40m'
+RED = '\033[1;31;40m'
+GREEN = '\033[1;32;40m'
+YELLOW = '\033[1;33;40m'
+BLUE = '\033[1;34;40m'
+MAG = '\033[1;35;40m'
+CYAN = '\033[1;36;40m'
+WHITE = '\033[1;37;40m'
+# -------------------------------------------------------------
+def acceptable_float_size(flt):
+  if flt > FLOAT_BTW[0] and flt < FLOAT_BTW[1]:
+    return True
+  else:
+    return False
+def flt_str_to_int(flt):
+  flt = flt.lower()
+  if 'k' in flt: # thousand
+    return int(float(flt[:-1])*1000)
+  elif 'm' in flt: # Million
+    return int(float(flt[:-1])*1000000)
+  else: # billion
+    return int(float(flt[:-1])*1000000000)
+def time_now():
+  return str(datetime.datetime.now())[:-7]
+HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'}
+def soup_maker(s):
+  req = Request(url=s, headers=HEADERS)
+  page = urlopen(req).read()
+  soup = BeautifulSoup(page, 'html.parser')
+  return soup
+def scrape_thestockmarketwatch():
+  soup = soup_maker(QUOTE_PAGE)
+  track_gainers = []
+  gainers = soup.find('table', {'id': 'tblMoversDesktop'})
+  for td in gainers.find_all('tr')[1:]:
+    perc = td.find('div', {'class':'chgUp'}).text[:-1]
+    last = td.find('div', {'class':'lastPrice'}).text[1:]
+    vol = td.find('td', {'class':'tdVolume'}).text
+    perc = float(perc)
+    last = float(last)
+    vol = int(vol)
+    if perc > GAINS_PERCENT and \
+       last < MAX_LAST and \
+       vol > MIN_VOLUME:
+      track_gainers.append(td)
+  return track_gainers
+def filter_yahoofin(candidates):
+  passed_candidates = []
+  for candidate in candidates:
+    ticker = candidate.find('a', {'class':'symbol'}).text
+    yahoo_fin = YAHOO_FIN.format(ticker, ticker)
+    soup = soup_maker(yahoo_fin)
+    flt = soup.find('td', {'class':'Fz(s) Fw(500) Ta(end) Pstart(10px) Miw(60px)'}).text
+    flt2 = flt_str_to_int(flt)
+    if acceptable_float_size(flt2):
+      passed_candidates.append((candidate,flt))
+  return passed_candidates
+def display(cds):
+  tab = tt.Texttable()
+  headings = ['Ticker','Name','Last','ChgUp','Float','Vol']
+  tab.header(headings)
+  for (c,flt) in cds:
+    chgUp = c.find('div', {'class':'chgUp'}).text
+    ticker = c.find('a', {'class':'symbol'}).text
+    coy_name = c.find('a', {'class':'company'}).text
+    volume = c.find('td', {'class':'tdVolume'}).text
+    last = c.find('div', {'class':'lastPrice'}).text
+    tab.add_row([ticker,coy_name,last,chgUp,flt,volume])
+  s = tab.draw()
+  return s
+  
+# -------------------------------------------------------------
+old_display = ''
+while 1:
+  eligible_candidates = scrape_thestockmarketwatch()
+  passed_candidates = filter_yahoofin(eligible_candidates)
+  new_display = display(passed_candidates)
+  if new_display != old_display:
+    print()
+    print('{}{} {}(delayed 15 mins, stops when market opens)'.format(RED,time_now(),WHITE))
+    print(new_display)
+    old_display = new_display
+  time.sleep(PERIOD)
+
